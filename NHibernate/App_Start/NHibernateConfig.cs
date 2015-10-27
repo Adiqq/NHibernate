00395042e0d598f0bd3e791330ffab22ab456f3e
Adrian Wawrzak <Adrian.Wawrzak@jcommerce.pl>
2015-10-27 15:01:34 +0100

    'init'

---
+++ NHibernate/App_Start/NHibernateConfig.cs
@@ -0,0 +1,46 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate.AspNet.Identity.Helpers;
+using NHibernate.Cfg;
+using NHibernate.Models;
+using NHibernate.Tool.hbm2ddl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NHibernate.App_Start
+{
+    public class NHibernateConfig
+    {
+        public static ISessionFactory CreateSessionFactory(bool createSchema = false)
+        {
+            return GetConfiguration().BuildSessionFactory();
+        }
+
+        private static FluentConfiguration GetConfiguration()
+        {
+            // this assumes you are using the default Identity model of "ApplicationUser"
+            var myEntities = new[] {
+                typeof(ApplicationUser)
+            };
+
+            var configuration = Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2012.ConnectionString(
+                    //c => c.FromAppSetting("connectionString")
+                    @"Data Source=ADWA;Initial Catalog=test;Integrated Security=True"
+                    ))
+                .ExposeConfiguration(cfg =>
+                {
+                    cfg.AddDeserializedMapping(
+                        MappingHelper.GetIdentityMappings(myEntities), null);
+                    /* Generacja początkowej struktury bazy
+
+                    new SchemaExport(cfg)
+                    .Create(false, true);
+                    */
+                });
+            return configuration;
+        }
+    }
+}
---
+++ NHibernate/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Mvc;
+using NHibernate.App_Start;
+using Owin;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+[assembly: OwinStartupAttribute(typeof(NHibernate.Startup))]
+namespace NHibernate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var container = UnityConfig.GetConfiguredContainer();
+
+            FilterProviders.Providers.Remove(FilterProviders.Providers.OfType<FilterAttributeFilterProvider>().First());
+            FilterProviders.Providers.Add(new UnityFilterAttributeFilterProvider(container));
+            
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+
+            ConfigureAuth(app);
+        }
+    }
+}
