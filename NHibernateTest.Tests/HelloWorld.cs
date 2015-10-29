79c3dd1de692f16ed0c5aea553f2835fdda867b6
Adrian Wawrzak <Adrian.Wawrzak@jcommerce.pl>
2015-10-29 14:31:48 +0100

    Tests library, folders added to ignored

---
+++ NHibernateTest.Tests/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernateTest.Tests
+{
+    [TestFixture]
+    public class HelloWorld
+    {
+        [Test]
+        public void Sample()
+        {
+            string text = "Hello world!";
+            Assert.AreEqual("Hello world!", text);
+        }
+    }
+}
